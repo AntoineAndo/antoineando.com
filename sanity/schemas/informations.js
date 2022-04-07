1a371663af40bde23fe45754ae3191853cac9d27
Antoine Ando <antoine.ando@gmail.com>
2022-04-07 15:35:39 +0200

    Text configuration via Sanity

--- sanity/schemas/informations.js
+++ sanity/schemas/informations.js
@@ -9,14 +9,197 @@ export default {
             type: "string"
         },
         {
+          name: 'nameConfiguration',
+          title: 'Name Configuration',
+          type: 'object',
+          fields: [
+            {
+              name: 'desktopPositions',
+              title: 'Desktop positions',
+              type: 'object',
+              fields: [
+                  {
+                      name: 'x',
+                      title: 'X',
+                      type: 'number'
+                  },
+                  {
+                      name: 'y',
+                      title: 'Y',
+                      type: 'number'
+                  },
+                  {
+                      name: 'z',
+                      title: 'Z',
+                      type: 'number'
+                  },
+                  {
+                      name: 'fontSize',
+                      title: 'Font size',
+                      type: 'number'
+                  }
+              ]
+            },
+            {
+                name: 'mobilePositions',
+                title: 'Mobile positions',
+                type: 'object',
+                fields: [
+                    {
+                        name: 'x',
+                        title: 'X',
+                        type: 'number'
+                    },
+                    {
+                        name: 'y',
+                        title: 'Y',
+                        type: 'number'
+                    },
+                    {
+                        name: 'z',
+                        title: 'Z',
+                        type: 'number'
+                    },
+                    {
+                        name: 'fontSize',
+                        title: 'Font size',
+                        type: 'number'
+                    }
+                ]
+            }
+          ]
+        },
+        {
             name: "jobTitle",
             title: "Job title",
             type: "string"
         },
         {
+            name: 'titleConfiguration',
+            title: 'Title Configuration',
+            type: 'object',
+            fields: [
+            {
+                name: 'desktopPositions',
+                title: 'Desktop positions',
+                type: 'object',
+                fields: [
+                    {
+                        name: 'x',
+                        title: 'X',
+                        type: 'number'
+                    },
+                    {
+                        name: 'y',
+                        title: 'Y',
+                        type: 'number'
+                    },
+                    {
+                        name: 'z',
+                        title: 'Z',
+                        type: 'number'
+                    },
+                    {
+                        name: 'fontSize',
+                        title: 'Font size',
+                        type: 'number'
+                    }
+                ]
+            },
+            {
+                name: 'mobilePositions',
+                title: 'Mobile positions',
+                type: 'object',
+                fields: [
+                    {
+                        name: 'x',
+                        title: 'X',
+                        type: 'number'
+                    },
+                    {
+                        name: 'y',
+                        title: 'Y',
+                        type: 'number'
+                    },
+                    {
+                        name: 'z',
+                        title: 'Z',
+                        type: 'number'
+                    },
+                    {
+                        name: 'fontSize',
+                        title: 'Font size',
+                        type: 'number'
+                    }
+                ]
+            }
+            ]
+        },
+        {
             name: "status",
             title: "Status",
             type: "string"
-        }
+        },
+        {
+            name: 'statusConfiguration',
+            title: 'Status Configuration',
+            type: 'object',
+            fields: [
+            {
+                name: 'desktopPositions',
+                title: 'Desktop positions',
+                type: 'object',
+                fields: [
+                    {
+                        name: 'x',
+                        title: 'X',
+                        type: 'number'
+                    },
+                    {
+                        name: 'y',
+                        title: 'Y',
+                        type: 'number'
+                    },
+                    {
+                        name: 'z',
+                        title: 'Z',
+                        type: 'number'
+                    },
+                    {
+                        name: 'fontSize',
+                        title: 'Font size',
+                        type: 'number'
+                    }
+                ]
+            },
+            {
+                name: 'mobilePositions',
+                title: 'Mobile positions',
+                type: 'object',
+                fields: [
+                    {
+                        name: 'x',
+                        title: 'X',
+                        type: 'number'
+                    },
+                    {
+                        name: 'y',
+                        title: 'Y',
+                        type: 'number'
+                    },
+                    {
+                        name: 'z',
+                        title: 'Z',
+                        type: 'number'
+                    },
+                    {
+                        name: 'fontSize',
+                        title: 'Font size',
+                        type: 'number'
+                    }
+                ]
+            }
+            ]
+        },
     ]
 }
